7f698294350ed3dbf90ac1ee576e446b9641262c
Manu Tyagi <manutygii90@gmail.com>
2025-01-24 16:50:34 +0530

    Delete Rest-API-CRUD-Operations1 directory

--- Rest-API-CRUD-Operations1/src/main/java/com/myrest/api/Repo/CloudRepo.java
+++
@@ -1,9 +0,0 @@
-package com.myrest.api.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.myrest.api.model.CloudVendor;
-
-public interface CloudRepo extends JpaRepository<CloudVendor, String> {
-	
-	CloudVendor findByVendorName(String vendorName);
-}
